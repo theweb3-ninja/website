d25ca7731208d87b4ba80156c13593a13f407d39
xdemocle <xdemocle@gmail.com>
2022-06-24 10:27:37 +0200

    refactoring

--- components/meta-header/meta-header.tsx
+++ components/meta-header/meta-header.tsx
@@ -1,67 +1,69 @@
-import Head from 'next/head';
+import Head from "next/head";
 
 const MetaHeader = () => {
-    return (
-        <Head>
-        <title>The Web3 Ninja | theweb3.ninja</title>
-        <meta name="description" content="The Web3 Ninja company website" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <link
-          rel="icon"
-          type="image/png"
-          href="/favicon-16x16.png"
-          sizes="16x16"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href="/favicon-32x32.png"
-          sizes="32x32"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href="/android-chrome-192x192.png"
-          sizes="192x192"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href="/android-chrome-512x512.png"
-          sizes="512x512"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <meta name="msapplication-TileColor" content="#B00F0A" />
-        <meta name="theme-color" content="#B00F0A" />
+  return (
+    <Head>
+      <title>The Web3 Ninja | theweb3.ninja</title>
+      <meta name="description" content="The Web3 Ninja company website" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      <link
+        rel="icon"
+        type="image/png"
+        href="/favicon-16x16.png"
+        sizes="16x16"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        href="/favicon-32x32.png"
+        sizes="32x32"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        href="/android-chrome-192x192.png"
+        sizes="192x192"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        href="/android-chrome-512x512.png"
+        sizes="512x512"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <meta name="msapplication-TileColor" content="#B00F0A" />
+      <meta name="theme-color" content="#B00F0A" />
 
-        <meta property="og:title" content="The Web3 Ninja | theweb3.ninja" />
-        <meta
-          property="og:description"
-          content="Your one stop man for your Web3 frontend integration."
-        />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://theweb3.ninja" />
-        <meta property="og:image:height" content="512" />
-        <meta property="og:image:width" content="512" />
-        <meta
-          property="og:image"
-          content="https://theweb3.ninja/android-chrome-512x512.png"
-        />
+      <meta property="og:title" content="The Web3 Ninja | theweb3.ninja" />
+      <meta
+        property="og:description"
+        content="Your one stop man for your Web3 frontend integration."
+      />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://theweb3.ninja" />
+      <meta property="og:image:height" content="512" />
+      <meta property="og:image:width" content="512" />
+      <meta
+        property="og:image"
+        content="https://theweb3.ninja/android-chrome-512x512.png"
+      />
 
-        <meta name="twitter:title" content="The Web3 Ninja | theweb3.ninja" />
-        <meta
-          name="twitter:description"
-          content="Your one stop man for your Web3 frontend integration."
-        />
-        <meta
-          name="twitter:image"
-          content="https://theweb3.ninja/android-chrome-512x512.png"
-        />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="google-site-verification" content="MTPnmdSrwTSlc4jluvZEaTDHKYchpR0FM8g4PoBYMHE" />
-        <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "ec261b44f7804ca1ba9c092912eadee1"}'></script>
-      </Head>
+      <meta name="twitter:title" content="The Web3 Ninja | theweb3.ninja" />
+      <meta
+        name="twitter:description"
+        content="Your one stop man for your Web3 frontend integration."
+      />
+      <meta
+        name="twitter:image"
+        content="https://theweb3.ninja/android-chrome-512x512.png"
+      />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta
+        name="google-site-verification"
+        content="MTPnmdSrwTSlc4jluvZEaTDHKYchpR0FM8g4PoBYMHE"
+      />
+    </Head>
   );
 };
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,49 @@
-import '../styles/fonts.scss'
-import '../styles/globals.scss'
-import type { AppProps } from 'next/app'
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-config.autoAddCss = false
+import "../styles/fonts.scss";
+import "../styles/globals.scss";
+import type { AppProps } from "next/app";
+import Script from "next/script";
+import * as gtag from "../lib/gtag";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const isProductionEnv = process.env.NODE_ENV !== "test";
+
+  return (
+    <>
+      <Component {...pageProps} />
+
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      {isProductionEnv && (
+        <>
+          <Script
+            strategy="afterInteractive"
+            src={`https://static.cloudflareinsights.com/beacon.min.js`}
+            data-cf-beacon='{"token": "ec261b44f7804ca1ba9c092912eadee1"}'
+          />
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+          />
+          <Script
+            id="gtag-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${gtag.GA_TRACKING_ID}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+            }}
+          />
+        </>
+      )}
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
